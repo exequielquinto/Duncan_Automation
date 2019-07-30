67e2127f4669b6973996ad2f63446aa6dacf8621
Lab5 <Lab5@DESKTOP-PUKPPRI>
2019-07-30 08:55:07 -0600

    update 1

--- Duncan_Thermals.py
+++ Duncan_Thermals.py
@@ -5,10 +5,13 @@ from D_Functions import dec_to_float32
 
 #Connect to Instruments
 rm = visa.ResourceManager()
-#daq = rm.open_resource('ASRL1::INSTR')
-client = ModbusClient(method = 'rtu' , port = 'COM9' , stopbits=1, parity ='N', baudrate='115200' ,timeout=0.5)
+daq = rm.open_resource('ASRL7::INSTR')
+client = ModbusClient(method = 'rtu' , port = 'COM10' , stopbits=1, parity ='N', baudrate='115200' ,timeout=0.5)
 connection = client.connect()
 #print(connection)
+
+#client2 = ModbusClient(method = 'rtu' , port = 'COM10' , stopbits=1, parity ='N', baudrate='115200' ,timeout=0.5)
+#connection2 = client2.connect()
 
 #FUNTIONS
 
@@ -52,29 +55,29 @@ def measure():
     temp['H_Vac_Out'] = Vac_Out
     
     #Measure Choke Backplate
-    #daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@101'))
-    #time.sleep(0.5)
-    #temp['I_Choke_Backplate_Temp'] = float(daq.read())
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@101'))
+    time.sleep(0.5)
+    temp['I_Choke_Backplate_Temp'] = float(daq.read())
     
     #Measure Choke Core
-    #daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@102'))
-    #time.sleep(0.5)
-    #temp['J_Choke_Core_Temp'] = float(daq.read())
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@102'))
+    time.sleep(0.5)
+    temp['J_Choke_Core_Temp'] = float(daq.read())
     
     #Measure Choke Top
-    #daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@103'))
-    #time.sleep(0.5)
-    #temp['K_Choke_Core_Temp'] = float(daq.read())
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@103'))
+    time.sleep(0.5)
+    temp['K_Choke_Top_Temp'] = float(daq.read())
     
     #Measure Trf Primary
-    #daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@104'))
-    #time.sleep(0.5)
-    #temp['L_Trf_Pri_Temp'] = float(daq.read())
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@104'))
+    time.sleep(0.5)
+    temp['L_Trf_Pri_Temp'] = float(daq.read())
 
     #Measure Trf Secondary
-    #daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@104'))
-    #time.sleep(0.5)
-    #temp['M_Trf_Pri_Temp'] = float(daq.read())    
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@105'))
+    time.sleep(0.5)
+    temp['M_Trf_Sec_Temp'] = float(daq.read())    
     
 results = pd.DataFrame()
 ref_time=time.time()
@@ -83,22 +86,25 @@ while 1:
     temp = {}
     #measure()
     try:
-        measure()
+        measure()        
     except:
         try:
             print('measure error1')
+            time.sleep(1)
             measure()
         except:
             try:
-                measure()
                 print('measure error2')
+                time.sleep(1)
                 measure()
             except:
                 print('measure error3')
+                time.sleep(1)
                 measure()
     
     results = results.append(temp, ignore_index=True)    # 17
-    print temp['A_Time'],'  ',temp['E_Pac_Grid'],'Watts','  ',temp['D_SOC'],'%'
+    print temp['A_Time'],' ',temp['E_Pac_Grid'],'Watts',' ',temp['C_PD'],' ',temp['D_SOC'],'%',' ',temp['I_Choke_Backplate_Temp'],'C',' ',temp['J_Choke_Core_Temp'],'C',' ',temp['K_Choke_Top_Temp'],'C',' ',temp['L_Trf_Pri_Temp'],'C',' ',temp['M_Trf_Sec_Temp'],'C'
+    #print temp2['A_Time'],' ',temp2['E_Pac_Grid'],'Watts',' ',temp2['C_PD'],' ',temp2['D_SOC'],'%',' ',temp2['I_Choke_Backplate_Temp'],'C',' ',temp2['J_Choke_Core_Temp'],'C',' ',temp2['K_Choke_Top_Temp'],'C',' ',temp2['L_Trf_Pri_Temp'],'C',' ',temp2['M_Trf_Sec_Temp'],'C'
     results.to_csv('Results.csv')
     time.sleep(60)   # Delay in seconds before capturing results               
 print('finished')
