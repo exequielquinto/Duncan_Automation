413ebd21905f305a0ca1d610387662318f5d4dfa
exequielquinto <42700497+exequielquinto@users.noreply.github.com>
2019-09-12 09:07:47 -0600

    Update Duncan_Thermals.py

--- Duncan_Thermals.py
+++ Duncan_Thermals.py
@@ -5,12 +5,15 @@ from D_Functions import dec_to_float32
 
 #Connect to Instruments
 rm = visa.ResourceManager()
-daq = rm.open_resource('ASRL18::INSTR')
-client = ModbusClient(method = 'rtu' , port = 'COM12' , stopbits=1, parity ='N', baudrate='115200' ,timeout=0.5)
+daq = rm.open_resource('ASRL13::INSTR')
+client = ModbusClient(method = 'rtu' , port = 'COM1' , stopbits=1, parity ='N', baudrate='115200' ,timeout=0.5)
 connection = client.connect()
 #print(connection)
 
 #FUNTIONS
+
+#print daq.write('TEMP:NPLC?',('@101:108'))
+#daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@101'))
 
 def measure():
     #Time
@@ -57,41 +60,64 @@ def measure():
     #temp['I_Choke_Backplate_Temp'] = float(daq.read())
     
     #Measure Choke Core
-    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@101'))
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@301'))
     time.sleep(0.5)
     temp['J_Choke_Core_Temp'] = float(daq.read())
     
     #Measure Choke Winding Bot
-    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@102'))
+    #daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@102'))
+    #time.sleep(0.5)
+    #temp['K_Choke_Winding_+Pad_Temp'] = float(daq.read())
+    
+    #Measure Choke Coil
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@303'))
     time.sleep(0.5)
-    temp['K_Choke_Winding_Bot_Temp'] = float(daq.read())
+    temp['L_Choke_Core_Top_Temp'] = float(daq.read())
     
     #Measure Choke Top
-    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@103'))
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@304'))
     time.sleep(0.5)
-    temp['L_Choke_Top_Temp'] = float(daq.read())
+    temp['M_Choke_Top_Temp'] = float(daq.read())
+    
+    #Measure Trf Pri
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@305'))
+    time.sleep(0.5)
+    temp['N_Trf_Pri_Temp'] = float(daq.read())
+    
+    #Measure Trf Sec
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@306'))
+    time.sleep(0.5)
+    temp['O_Trf_Sec_Temp'] = float(daq.read())
+    
+    #Measure Trf Pri Wire
+    #daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@107'))
+    #time.sleep(0.5)
+    #temp['P_Trf_Pri_Wire_Temp'] = float(daq.read())
+    
+    #Measure Mag Assy Int Amb
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@307'))
+    time.sleep(0.5)
+    temp['P_Mag_Assy_Int_Temp'] = float(daq.read())
     
     #Measure T ambient
-    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@104'))
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@308'))
     time.sleep(0.5)
-    temp['N_T_Amb'] = float(daq.read()) 
+    temp['Q_T_Amb'] = float(daq.read()) 
     
     #Measure Internal Temp
     response = client.read_input_registers(5093,2,unit=1)
     Int_Temp=dec_to_float32(response.registers[0], response.registers[1])
-    temp['O_Int_Temp'] = Int_Temp
+    temp['R_Int_Temp'] = Int_Temp
     
-    #Measure Choke Winding Pad
-    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@105'))
+    #Measure Trf Center
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@309'))
     time.sleep(0.5)
-    temp['P_Choke_Pad_Temp'] = float(daq.read())
-
-    #Measure Int Ambient
-    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@106'))
+    temp['S_Trf_Center'] = float(daq.read())
+    
+    #Measure Int Amb
+    daq.write('MEAS:TEMP? %s,%s,(%s)' % ('TCouple', 'K', '@309'))
     time.sleep(0.5)
-    temp['Q_Int_Ambient_TC'] = float(daq.read())    
-
-
+    temp['T_Int_Amb'] = float(daq.read())
     
 results = pd.DataFrame()
 ref_time=time.time()
@@ -119,7 +145,7 @@ while temp['C_PD'] !=1:
     
     results = results.append(temp, ignore_index=True)    # 17
     #print temp['A_Time'],' ',temp['E_Pac_Grid'],'Watts',' ',temp['C_PD'],' ',temp['D_SOC'],'%',' ',temp['I_Choke_Backplate_Temp'],'C',' ',temp['J_Choke_Core_Temp'],'C',' ',temp['K_Choke_Top_Temp'],'C',' ',temp['L_Trf_Pri_Temp'],'C',' ',temp['M_Trf_Sec_Temp'],'C'
-    print temp['A_Time'],' ',temp['E_Pac_Grid'],'Watts',' ',temp['C_PD'],' ',temp['D_SOC'],'%',' ',temp['J_Choke_Core_Temp'],'C',' ',temp['K_Choke_Winding_Bot_Temp'],'C',' ',temp['L_Choke_Top_Temp'],'C',' ',temp['N_T_Amb'],'C',temp['O_Int_Temp'],'C',temp['P_Choke_Pad_Temp'],'C',' ',temp['Q_Int_Ambient_TC'],'C'
-    results.to_csv('Unit2 with Option A shortened backplate Charge Discharge Additional Data.csv')
+    print temp['A_Time'],' ',temp['E_Pac_Grid'],'Watts',' ',temp['C_PD'],' ',temp['D_SOC'],'%',' ',temp['J_Choke_Core_Temp'],'C',' ',temp['L_Choke_Core_Top_Temp'],'C',' ',temp['M_Choke_Top_Temp'],'C',temp['N_Trf_Pri_Temp'],'C',temp['O_Trf_Sec_Temp'],'C',' ',temp['P_Mag_Assy_Int_Temp'],'C',' ',temp['S_Trf_Center'],'C',' ',temp['Q_T_Amb'],'C',' ',temp['R_Int_Temp'],'C',' ',temp['T_Int_Amb'],'C'
+    results.to_csv('Unit2 with Option B and Trf 03 Charge Discharge for New Fire Encl 2.csv')
     time.sleep(60)   # Delay in seconds before capturing results               
 print('finished')
